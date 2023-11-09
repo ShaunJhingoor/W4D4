176fded2bd32ac40f2e1dd20620f90adb638158a
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-09 17:10:39 -0500

    finish anagram

--- anagrams.rb
+++ anagrams.rb
@@ -23,10 +23,29 @@ def second_anagram?(str1, str2)
     end 
     array.empty?
 end 
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
 
-# if __FILE__ == $0
-#     foo()
-#     bar()
-# end
+def third_anagram?(str1, str2)
+    str1.chars.sort == str2.chars.sort 
+end
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+
+def fourth_anagram?(str1,str2)
+    hash1 = Hash.new(0)
+    hash2 = Hash.new(0)
+    str1.each_char{|char| hash1[char] += 1}
+    str2.each_char{|char| hash2[char] += 1}
+    hash1 == hash2 
+end
+# p fourth_anagram?("gizmo", "sally")    #=> false
+# p fourth_anagram?("elvis", "lives")    #=> true
+
+def bonus?(str1, str2)
+    hash = Hash.new(0)
+    str1.each_char{|char| hash[char] += 1}
+    str2.each_char{|char| hash[char] -= 1}
+    hash.empty? 
+end
+# p bonus?("gizmo", "sally")    #=> false
