282a8dd4dd11be5cf49a4c21b2214f3659ef384d
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-09 14:28:22 -0500

    Finish part 1

--- EXECUTION_TIME_DIFFERENCE.rb
+++ EXECUTION_TIME_DIFFERENCE.rb
@@ -1,4 +1,22 @@
-def my_min(arr)         #O(n) - linear
+def my_min(arr) # O(n^2)
+    sorted = true 
+    while sorted 
+        sorted = false 
+        (0...arr.length - 1).each do |i|
+            if arr[i] > arr[i + 1]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                sorted = true 
+            end
+        end
+    end
+    arr[0]
+end
+
+       
+list = [0, 3, 5, 4, -5, 10, 1, 90]
+p my_min(list) #=> -5
+
+def my_min_quick(arr)         #O(n) - linear
     smallest = arr.first
     arr.each do |ele|
         if ele < smallest
@@ -8,6 +26,4 @@ def my_min(arr)         #O(n) - linear
     smallest
 end
 
-list = [0, 3, 5, 4, -5, 10, 1, 90]
-p my_min(list) #=> -5
 
